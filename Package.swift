2fd18ece6370c32f47ec90e49c2f9340b5bcafef
Steven Lambion <steven.lambion@atsid.com>
2019-06-05 15:23:31 -0700

    Added supported platforms for use by Xcode 11.

--- Package.swift
+++ Package.swift
@@ -1,28 +1,31 @@
-// swift-tools-version:4.2
+// swift-tools-version:5.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "GRDB",
+    platforms: [
+      .iOS(.v9), .macOS(.v10_10), .watchOS(.v2), .tvOS(.v9),
+    ],
     products: [
-        .library(name: "GRDB", targets: ["GRDB"]),
+      .library(name: "GRDB", targets: ["GRDB"]),
     ],
-    dependencies: [
-        .package(url: "https://github.com/groue/CSQLite.git", from: "0.2.0"),
+     dependencies: [
+      .package(url: "https://github.com/groue/CSQLite.git", from: "0.2.0"),
     ],
-    targets: [
-        .target(name: "GRDB", path: "GRDB"),
-        .testTarget(
-            name: "GRDBTests",
-            dependencies: ["GRDB"],
-            path: "Tests",
-            exclude: [
-                "CocoaPods",
-                "Crash",
-                "Performance",
-                "SPM"
-            ])
-    ],
-    swiftLanguageVersions: [.v4_2, .version("5")]
+     targets: [
+      .target(name: "GRDB", path: "GRDB"),
+      .testTarget(
+        name: "GRDBTests",
+        dependencies: ["GRDB"],
+        path: "Tests",
+        exclude: [
+          "CocoaPods",
+          "Crash",
+          "Performance",
+          "SPM"
+        ])
+  ],
+    swiftLanguageVersions: [.v4_2, .v5]
 )
